42df514fceb0b327ec9dc58a305473b4a29ca7b7
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-06-02 21:09:50 +0700

    refactor: remove unused components and styles, update Tailwind CSS integration
    - Removed App.css, Form, Result, Navbar components, and their respective assets.
    - Deleted postcss.config.js and output.css for cleaner project structure.
    - Updated package.json to use @tailwindcss/vite and removed unused dependencies.
    - Added styles.css to import Tailwind CSS directly.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,9 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import Navbar from './components/navbar';
-import Form from './components/form';
-import Result from './components/result';
-import './output.css';
+import React from 'react'
 
-function App() {
-  const [urlResult, setUrlResult] = useState("");
-
-  const handleFormSubmit = (url) => {
-      setUrlResult(url);
-  };
-
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <div className="flex min-h-screen bg-gray-100 gap-4 p-6">
-        <div className="max-w-2xl">
-          <Form onSubmit={handleFormSubmit} />
-        </div>
-
-        {/* Result selalu tampil, tapi teksnya tergantung urlResult */}
-        <div className="flex-grow">
-          <Result value={urlResult} />
-        </div>
-      </div>
-      
-      <div className="App flex justify-center pt-16">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-gray-900'>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs text-gray-900">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    <></>
+  )
 }
 
-export default App;
+export default App
