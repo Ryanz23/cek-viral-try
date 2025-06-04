2da48af37d0506538c687350c5d4484ae0d3f199
ryanz23 <ariyanandryan@gmail.com>
2025-06-04 14:54:41 +0700

    change padding, size and add login navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,34 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image from "../assets/react.svg";
+import image from "../assets/LOGO.svg";
 const navigation = () => {
   return (
-    <header className="bg-white p-3 shadow-sm fixed top-0 w-full z-50">
-      <nav className="flex items-center justify-between w-7xl m-auto ">
+    <header className="bg-white p-6 shadow-sm fixed top-0 h-18 w-full z-50">
+      <nav className="flex items-center justify-between w-7xl mx-auto ">
         <div>
-          <img src={image} alt="logo" />
+          <img src={image} alt="logo" className="" />
         </div>
-        <ul className="flex gap-3 ">
+        <ul className="flex gap-3 text-sm">
           <li>
             <Link
               to="/"
-              className="text-gray-600 hover:text-red-600 transition-all"
+              className="text-gray-600 hover:text-blue-500 transition-all"
             >
-              HOME
+              BERANDA
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/how-it-works"
+              className="text-gray-600 hover:text-blue-500 transition-all"
+            >
+              CARA KERJA
             </Link>
           </li>
           <li>
             <Link
               to="/about"
-              className="text-gray-600 hover:text-amber-300 transition-all"
+              className="text-gray-600 hover:text-blue-500 transition-all"
             >
-              ABOUT
+              TENTANG
             </Link>
           </li>
         </ul>
-        <ul>
+        <ul className="flex gap-3">
           <li>
-            <Link to="/regist" className="text-gray-600 hover:text-amber-300 transition-all">REGIST</Link>
+            <Link to="/login" className="bg-gradient-to-r from-blue-main to-green-main bg-clip-text text-transparent text-sm hover:text-blue-500 transition-all">MASUK</Link>
+          </li>
+          <li>
+            <Link to="/regist" className="bg-gradient-to-r from-blue-main to-green-main bg-clip-text text-transparent text-sm hover:text-blue-500 transition-all">DAFTAR</Link>
           </li>
         </ul>
       </nav>
