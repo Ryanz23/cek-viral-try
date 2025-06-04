fbd8aeb29934f278e0158a3d050b7e2c65bede12
ryanz23 <ariyanandryan@gmail.com>
2025-06-04 14:56:46 +0700

    add file Works.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navigation from "./components/navigation";
 import Home from "./pages/Home";
+import Works from "./pages/Works";
 import About from "./pages/About";
 import PageTransition from "./components/PageTransition";
 import Regist from "./pages/Regist";
@@ -15,6 +16,7 @@ const App = () => {
         <PageTransition>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/how-it-works" element={<Works />} />
             <Route path="/about" element={<About />} />
             <Route path="/regist" element={<Regist/>}/>
             <Route path="/result" element={<Result/>}/>
