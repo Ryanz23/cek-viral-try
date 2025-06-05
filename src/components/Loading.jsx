8e89c13b7e694b4d028704e30d2bdf51c621fd97
ryanz23 <ariyanandryan@gmail.com>
2025-06-05 08:13:49 +0700

    make loading indicator with toast

---
+++ src/components/Loading.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { toast } from 'react-hot-toast'; // atau library toast yang Anda pakai
+
+const Loading = () => {
+  const showLoadingToast = (message = "Silakan tunggu, kami sedang memproses...") => {
+    return toast.success(message, {
+      duration: 2000,
+      position: 'top-center',
+    });
+  };
+
+  const handleLoadingSubmit = (callback, delay = 2000) => {
+    showLoadingToast();
+    
+    setTimeout(() => {
+      callback();
+    }, delay);
+  };
+
+  return { showLoadingToast, handleLoadingSubmit };
+};
+
+export default Loading;
