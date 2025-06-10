72862d0b14955c7513a62ff8807f656d5046a5a5
ryanz23 <ariyanandryan@gmail.com>
2025-06-10 21:41:02 +0700

    fixing conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,4 +6,3 @@ module.exports = {
   },
   plugins: [],
 };
->>>>>>> main
